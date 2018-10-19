7ebfb53dcc3dbdb3744c8ce70cc58b3e6d525942
Adam Pod <adam.podoxin@gmail.com>
2018-10-18 17:52:05 -0700

    Added path and bpm fields for level setup

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -10,6 +10,8 @@ public class LevelGenerator : MonoBehaviour
     public Text nameText;
     public Text sizeXText;
     public Text sizeYText;
+    public InputField pathText;
+    public Text bpmText;
 
     private GridGenerator gridGenerator;
 
@@ -20,7 +22,10 @@ public class LevelGenerator : MonoBehaviour
 
     public void CreateLevel()
     {
-        level = new Level(nameText.text, new Vector2(int.Parse(sizeXText.text), int.Parse(sizeYText.text)));
+        WWW www = new WWW("file://" + pathText.text);
+        AudioClip song = www.GetAudioClip();
+
+        level = new Level(nameText.text, new Vector2(int.Parse(sizeXText.text), int.Parse(sizeYText.text)), song, int.Parse(bpmText.text));
         gridGenerator.GenerateGrid(level.size);
     }
 }
@@ -30,15 +35,19 @@ public class Level
 {
     public string name = "New Level";
     public Vector2 size = Vector2.one * 13f;
+    public AudioClip song;
+    public int bpm;
 
     public Level()
     {
 
     }
 
-    public Level(string name, Vector2 size)
+    public Level(string name, Vector2 size, AudioClip song, int bpm)
     {
         this.name = name;
         this.size = size;
+        this.song = song;
+        this.bpm = bpm;
     }
 }
