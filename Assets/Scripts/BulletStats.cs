86f72e374eee5861793a8ed7271e507517d9fb43
Adam Pod <adam.podoxin@gmail.com>
2018-10-18 18:30:09 -0700

    Changed Bullet to BulletStats. Changed bullet variable to string bulletType

---
+++ Assets/Scripts/BulletStats.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStats : MonoBehaviour
+{
+    public string bulletType;
+    public Vector2 direction;
+}
