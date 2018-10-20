167dfce14843ec007dae2d90285d0fd711636c2d
Adam Pod <adam.podoxin@gmail.com>
2018-10-19 20:21:14 -0700

    Created frame window UI

---
+++ Assets/Scripts/FrameWindow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrameWindow : MonoBehaviour
+{
+    public static FrameWindow instance;
+
+    public Text levelTitleText;
+    public Text frameNumberText;
+
+    private LevelGenerator levelGenerator;
+
+    public void Start()
+    {
+        instance = this;
+        levelGenerator = LevelGenerator.instance;
+
+        Setup();
+    }
+
+    public void Setup()
+    {
+        levelTitleText.text = LevelGenerator.instance.level.name;
+        frameNumberText.text = "Frame #" + (levelGenerator.currentFrame + 1) + "/" + levelGenerator.level.amountOfFrames;
+    }
+}
