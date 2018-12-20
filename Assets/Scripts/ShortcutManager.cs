20886a90c48cdce144b2fb1460265768bc780a45
Adam Pod <adam.podoxin@gmail.com>
2018-12-19 19:28:29 -0800

    Shortcuts don't activate if typing

--- Assets/Scripts/ShortcutManager.cs
+++ Assets/Scripts/ShortcutManager.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShortcutManager : MonoBehaviour
 {
     public static ShortcutManager instance;
 
+    public InputField saveField;
+    public InputField specialtyNumberField;
+
     private LevelGenerator levelGenerator;
     private PropertiesWindow propertiesWindow;
+
+    private bool canActivate = true;
 
     private void Start()
     {
@@ -21,71 +27,76 @@ public class ShortcutManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (canActivate)
         {
-            levelGenerator.FrameUp();
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            levelGenerator.FrameDown();
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            levelGenerator.AddFrame();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            levelGenerator.RemoveFrame();
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                levelGenerator.FrameUp();
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                levelGenerator.FrameDown();
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                levelGenerator.AddFrame();
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                levelGenerator.RemoveFrame();
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                propertiesWindow.SelectMode("Select");
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                propertiesWindow.SelectMode("Draw");
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                propertiesWindow.SelectMode("Remove");
+            }
+
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                propertiesWindow.SetDirection("None");
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                propertiesWindow.SetDirection("Up-Left");
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                propertiesWindow.SetDirection("Up");
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                propertiesWindow.SetDirection("Up-Right");
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                propertiesWindow.SetDirection("Right");
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                propertiesWindow.SetDirection("Down-Right");
+            }
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                propertiesWindow.SetDirection("Down");
+            }
+            else if (Input.GetKeyDown(KeyCode.Z))
+            {
+                propertiesWindow.SetDirection("Down-Left");
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                propertiesWindow.SetDirection("Left");
+            }
         }
 
-        else if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            propertiesWindow.SelectMode("Select");
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            propertiesWindow.SelectMode("Draw");
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            propertiesWindow.SelectMode("Remove");
-        }
-
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            propertiesWindow.SetDirection("None");
-        }
-        else if (Input.GetKeyDown(KeyCode.Q))
-        {
-            propertiesWindow.SetDirection("Up-Left");
-        }
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            propertiesWindow.SetDirection("Up");
-        }
-        else if (Input.GetKeyDown(KeyCode.E))
-        {
-            propertiesWindow.SetDirection("Up-Right");
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            propertiesWindow.SetDirection("Right");
-        }
-        else if (Input.GetKeyDown(KeyCode.C))
-        {
-            propertiesWindow.SetDirection("Down-Right");
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            propertiesWindow.SetDirection("Down");
-        }
-        else if (Input.GetKeyDown(KeyCode.Z))
-        {
-            propertiesWindow.SetDirection("Down-Left");
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            propertiesWindow.SetDirection("Left");
-        }
+        canActivate = saveField.isFocused == false && specialtyNumberField.isFocused == false;
     }
 }
